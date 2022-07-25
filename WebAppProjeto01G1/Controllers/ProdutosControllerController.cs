3459d1040a861472b90c6eef0d515f863031e45b
Sérgio Júnior <sergiojr9834@gmail.com>
2022-07-25 08:30:49 -0300

    Inclusão da classe Produto

---
+++ WebAppProjeto01G1/Controllers/ProdutosControllerController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto01G1.Models;
+
+namespace WebAppProjeto01G1.Controllers
+{
+    public class ProdutosControllerController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: ProdutosController
+        public ActionResult Index()
+        {
+            return View(context.Produtos.OrderBy(c => c.Nome));
+        }
+
+        // GET: ProdutosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProdutosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProdutosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProdutosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProdutosController/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
