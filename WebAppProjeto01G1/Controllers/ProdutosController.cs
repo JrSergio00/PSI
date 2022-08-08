50d6c777bb3cad43c230f1b0e25d291822533688
Sérgio Júnior <sergiojr9834@gmail.com>
2022-08-08 10:21:36 -0300

    Criação do details e delete da categoria Produtos

--- WebAppProjeto01G1/Controllers/ProdutosController.cs
+++ WebAppProjeto01G1/Controllers/ProdutosController.cs
@@ -21,10 +21,20 @@ namespace WebAppProjeto01G1.Controllers
             return View(produtos);
         }
 
-        // GET: ProdutosController/Details/5
-        public ActionResult Details(int id)
+        // GET: Produtos/Details/5
+        public ActionResult Details(long? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.Produtos.Where(p => p.ProdutoId == id).
+            Include(c => c.Categoria).Include(f => f.Fabricante).First();
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
         }
 
         // GET: ProdutosController/Create
@@ -89,20 +99,32 @@ namespace WebAppProjeto01G1.Controllers
             }
         }
 
-        // GET: ProdutosController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(long? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = context.Produtos.Where(p => p.ProdutoId == id).
+            Include(c => c.Categoria).Include(f => f.Fabricante).First();
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
         }
 
-        // POST: ProdutosController/Delete/5
+        // POST: Produtos/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
             {
-                // TODO: Add delete logic here
-
+                Produto produto = context.Produtos.Find(id);
+                context.Produtos.Remove(produto);
+                context.SaveChanges();
+                TempData["Message"] = "Produto " + produto.Nome.ToUpper() + " foi removido";
                 return RedirectToAction("Index");
             }
             catch
