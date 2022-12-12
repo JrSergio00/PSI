e14a240a73132b9f0376ceef9807e16fbda91148
20211011110037 <20211011110037@CNAT391782>
2022-12-12 08:25:21 -0300

    Alterações

--- Modelo/Cadastros/Produto.cs
+++ Modelo/Cadastros/Produto.cs
@@ -28,5 +28,7 @@ namespace Modelo.Cadastros
         public byte[] Logotipo { get; set; }
         public Categoria Categoria { get; set; }
         public Fabricante Fabricante { get; set; }
+        public string NomeArquivo { get; set; }
+        public long TamanhoArquivo { get; set; }
     }
 }
