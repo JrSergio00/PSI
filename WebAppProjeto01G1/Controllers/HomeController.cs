8be0e5de18a6385ab2b5fec90349e5e3a6804853
Jrsergio00 <junior.sergio@escolar.ifrn.edu.br>
2022-12-15 05:07:36 -0300

    Inclusão da lista de produtos na Home

--- WebAppProjeto01G1/Controllers/HomeController.cs
+++ WebAppProjeto01G1/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Modelo.Cadastros;
+using Modelo.Tabelas;
+using Servico.Cadastros;
+using Servico.Tabelas;
+using System.Net;
+using System.IO;
 
 namespace WebAppProjeto01G1.Controllers
 {
     public class HomeController : Controller
     {
+        private FabricanteServico fabricanteServico = new FabricanteServico();
+        private ProdutoServico produtoServico = new ProdutoServico();
+        private CategoriaServico categoriaServico = new CategoriaServico();
+        
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(produtoServico.ObterProdutosClassificadosPorNome());
         }
     }
 }
